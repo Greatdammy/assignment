7d0a09331c1a85865d849a09d328084c999b2c8c
H0me1 <H0me1@OBAFEMI>
2022-04-05 22:59:06 +0100

    Add project files.

---
+++ first.assignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace first.assignment
+{
+    public class Book
+    {
+        public string title;
+        public string author;
+        public string ISBN;
+
+
+        public void ReadBook()
+        {
+            Console.WriteLine("This book is readable. ");
+        }
+
+        public void ShelveBook()
+        {
+            Console.WriteLine("This book has been shelved ");
+        }
+    }
+
+
+
+    public class Program
+    {
+       public static void Main()
+        {
+            Book _book = new Book();
+
+            _book.title = "How you can be led by the Holy spirit";
+            _book.author = "Kenneth Hagin";
+            _book.ISBN = "F978-0-448-48423-6";
+
+            _book.ReadBook();
+            Console.WriteLine("Title: {0}\nAuthor: {1}\nISBN: {2}", _book.title, _book.author, _book.ISBN);
+
+          
+
+            Book _book2 = new Book();
+
+            _book2.title = "The theory about C#";
+            _book2.author = "Obafemi Damilare";
+            _book2.ISBN = "D124-422-4323-2";
+
+            _book.ShelveBook();
+            Console.WriteLine("Title: {0}\nAuthor: {1}\nISBN: {2}", _book2.title, _book2.author, _book2.ISBN);
+        }
+    }
+}
---
+++ second.Assignment/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace second.Assignment
+{
+
+    public class Vehicle
+    {
+        public string brandName = "FORD";
+
+        public void DisplayVehicleType(string vehicleType)
+        {
+            Console.WriteLine("This is a {0}", vehicleType);
+        }
+    }
+    
+
+    public class Car : Vehicle
+    {
+        public string vehicleType = "Car";
+
+        public void GetBrandName()
+        {
+            Console.WriteLine("Brand: {0}", brandName);
+        }
+           
+    }
+      
+    public class Truck : Vehicle
+    {
+        public string vehicleType = "Truck";
+
+        public void GetBrandName()
+            {
+            Console.WriteLine("Brand: {0}", brandName);
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public string vehicleType = "Motorcycle";
+
+        public void GetBrandName()
+        {
+            Console.WriteLine("Brand: {0}", brandName);
+        }
+    }
+    
+    public class Program
+    {
+       public static void Main()
+        {
+            Car _suv = new Car();
+            _suv.DisplayVehicleType("Explorer");
+            _suv.GetBrandName();
+
+
+            Truck truck = new Truck();
+            truck.DisplayVehicleType("Ford ranger");
+            truck.GetBrandName();
+
+            Motorcycle motorcycle = new Motorcycle();
+            motorcycle.DisplayVehicleType("Cleveland");
+            motorcycle.GetBrandName();
+        }
+    }
+}
